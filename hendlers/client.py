4aa0cad44e29613703986c638c80853c28e821df
Esen <esenbekm03@gmail.com>
2022-05-10 19:57:21 +0600

    lesson 2

---
+++ hendlers/client.py
@@ -0,0 +1,41 @@
+from aiogram import types, Dispatcher
+from config import bot, dp
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
+from keyboards import client_kb
+
+
+# @dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await message.answer(f"Привет мой хозяин {message.from_user.full_name}",
+                         reply_markup=client_kb.start_markup)
+
+
+async def help(message: types.Message):
+    await message.answer(f"ННСТРУКЦИЯ")
+
+
+# @dp.message_handler(commands=['quiz'])
+async def quiz_1(message: types.Message):
+    markup = InlineKeyboardMarkup()
+    button_call_1 = InlineKeyboardButton("NEXT", callback_data="button_call_1")
+    markup.add(button_call_1)
+
+    question = "Сколько будет 1 + 1 ?"
+    answers = ['1', '2', '3', '4', '322']
+    await bot.send_poll(
+        chat_id=message.from_user.id,
+        question=question,
+        options=answers,
+        is_anonymous=False,
+        type='quiz',
+        correct_option_id=1,
+        explanation="Это же легко",
+        explanation_parse_mode=ParseMode.MARKDOWN_V2,
+        reply_markup=markup
+    )
+
+
+def register_handler_client(dp: Dispatcher):
+    dp.register_message_handler(start, commands=['start'])
+    dp.register_message_handler(help, commands=['help'])
+    dp.register_message_handler(quiz_1, commands=['quiz'])
--- main.py
+++ main.py
@@ -1,93 +1,12 @@
-from aiogram import types, Bot, Dispatcher
 from aiogram.utils import executor
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
+from config import dp
+from hendlers import admin, client, callback, extra
 import logging
-from decouple import config
 
-TOKEN = config("TOKEN")
-bot = Bot(TOKEN)
-dp = Dispatcher(bot=bot)
-
-
-@dp.message_handler(commands=['start'])
-async def start(message: types.Message):
-    await message.answer(f"Привет мой хозяин {message.from_user.full_name}")
-
-
-@dp.message_handler(commands=['quiz'])
-async def quiz_1(message: types.Message):
-    markup = InlineKeyboardMarkup()
-    button_call_1 = InlineKeyboardButton("NEXT", callback_data="button_call_1")
-    markup.add(button_call_1)
-
-    question = "Сколько будет 1 + 1 ?"
-    answers = ['1', '2', '3', '4', '322']
-    await bot.send_poll(
-        chat_id=message.from_user.id,
-        question=question,
-        options=answers,
-        is_anonymous=False,
-        type='quiz',
-        correct_option_id=1,
-        explanation="Это же легко",
-        explanation_parse_mode=ParseMode.MARKDOWN_V2,
-        reply_markup=markup
-    )
-
-@dp.callback_query_handler(lambda call: call.data == "button_call_1")
-async def quiz_2(call: types.CallbackQuery):
-    markup = InlineKeyboardMarkup()
-    button_call_2 = InlineKeyboardButton("NEXT", callback_data="button_call_2")
-    markup.add(button_call_2)
-
-    question = "Кто создатель ЯП Python?"
-    answers = [
-        'Volendemord',
-        'Nurgazy',
-        'Pikachu',
-        'Guido Van Rossum',
-        'Elon Mask'
-    ]
-    await bot.send_poll(
-        chat_id=call.from_user.id,
-        question=question,
-        options=answers,
-        is_anonymous=False,
-        type='quiz',
-        correct_option_id=3,
-        explanation="Это же легко",
-        explanation_parse_mode=ParseMode.MARKDOWN_V2,
-        reply_markup=markup
-    )
-
-@dp.callback_query_handler(lambda call: call.data == "button_call_2")
-async def quiz_2(call: types.CallbackQuery):
-
-    question = "Ответы:"
-    answers = [
-        '4',
-        '8',
-        '4, 6',
-        '2, 4'
-        '5',
-    ]
-    photo = open("media/problem1.jpg", "rb")
-    await bot.send_photo(call.from_user.id, photo=photo)
-    await bot.send_poll(
-        chat_id=call.from_user.id,
-        question=question,
-        options=answers,
-        is_anonymous=False,
-        type='quiz',
-        correct_option_id=3,
-        explanation="Это же легко",
-        explanation_parse_mode=ParseMode.MARKDOWN_V2,
-    )
-
-@dp.message_handler()
-async def echo(message: types.Message):
-    await bot.send_message(message.from_user.id, message.text)
-
+client.register_handler_client(dp)
+admin.register_handlers_admin(dp)
+callback.register_handler_callback(dp)
+extra.register_hendler_extra(dp)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
