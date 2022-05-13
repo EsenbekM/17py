a029e96d08a73f0f34438edfb9034a46aa7e3df3
Esen <esenbekm03@gmail.com>
2022-05-13 19:46:00 +0600

    Lesson 3

--- keyboards/client_kb.py
+++ keyboards/client_kb.py
@@ -8,3 +8,8 @@ info_button = KeyboardButton("Share info", request_contact=True)
 start_markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 
 start_markup.row(help_button, quiz_button, location_button, info_button)
+
+cancel_button = KeyboardButton("CANCEL")
+cancel_marcup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+
+cancel_marcup.add(cancel_button)
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from aiogram.utils import executor
 from config import dp
-from hendlers import admin, client, callback, extra
+from hendlers import admin, client, callback, extra, fsm_anketa
 import logging
 
 client.register_handler_client(dp)
 admin.register_handlers_admin(dp)
+fsm_anketa.register_hendler_fsmanketa(dp)
 callback.register_handler_callback(dp)
 extra.register_hendler_extra(dp)
 
